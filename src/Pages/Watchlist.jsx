cfe712c3518a39d422bb2cbd8a9c2abca1e069fb
Kalifa <128218748+Kalifa-A@users.noreply.github.com>
2026-03-01 14:37:49 +0530

    Update Watchlist.jsx

--- src/Pages/Watchlist.jsx
+++ src/Pages/Watchlist.jsx
@@ -12,7 +12,7 @@ export default function Watchlist() {
   const token = localStorage.getItem('token');
 
   // Define API Base
-  const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+  const API_BASE ='http://localhost:5000/api';
 
   useEffect(() => {
     if (!token) {
@@ -149,3 +149,4 @@ export default function Watchlist() {
   );
 }
 export { Watchlist };
+
